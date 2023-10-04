9547b7f8b1ae0eeb0ddead5246ed65e48bfe20e3
itdelta-prog <delta@it-delta.ru>
2023-10-04 15:33:03 +0300

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,13 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         contact: resolve(__dirname, 'pages/contact.html')
-      }
+      },
+      output: {
+        manualChunks: false,
+      //  inlineDynamicImports: true,
+        entryFileNames: '[name].js',   // currently does not work for the legacy bundle
+        assetFileNames: '[name].[ext]', // currently does not work for images
+      },
     }
   }
 });
