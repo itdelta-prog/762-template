e41ad9a2985e19585d480e3402fc23cfd6ad3c3d
itdelta-prog <delta@it-delta.ru>
2023-10-13 16:56:11 +0300

    arsenal page

--- src/js/arsenal.js
+++ src/js/arsenal.js
@@ -1,17 +1,12 @@
 
-
-function minusShort() {
-
-}
+const formGung = document.getElementById('gungForm');
 const shortCurrent = () => {
     const shortWrapper = document.querySelector('.current-shot');
-    const formGung = document.getElementById('gungForm');
     const contentShort = document.querySelector('.btnContent');
     const shortValues = 2550;
     let shortActive = undefined;
     function shortPrice(short) {
 
-        console.log(short?.dataset.minus);
         if(short?.dataset.minus) {
            return contentShort.innerHTML = `${shortValues - Number(short.dataset.minus)} выстрелов`;
         }
@@ -19,19 +14,9 @@ const shortCurrent = () => {
           return  contentShort.innerHTML = `${shortValues + Number(short.dataset.plus)} выстрелов`;
         }
         else {
-            console.log('ok')
           return contentShort.innerHTML = `${shortValues} выстрелов`;
         }
-
-
-        // contentShort.dataset.contentshort = Number(contentShort.dataset.contentshort) + Number(eve.target.dataset.currentshort)
-        // return contentShort.innerHTML = `${Number(contentShort.dataset.contentshort) + Number(eve.target.dataset.currentshort)} выстрелов`;
     }
-
-
-    formGung.addEventListener('click', (eve) => {
-        eve.preventDefault();
-    })
 
     shortWrapper.addEventListener('click', (eve) => {
         if(eve.target.className.includes('btnCurrent')) {
@@ -50,6 +35,23 @@ const shortCurrent = () => {
         }
     })
 }
+const personShort = () => {
+    const personWrapper = document.querySelector('.person');
+    let activeElement = document.querySelector('.person-active');
+    personWrapper.addEventListener('click', (eve) => {
+        if(eve.target.className.includes('btnCurrent')) {
+            if(activeElement) {
+                activeElement.classList.remove('person-active')
+            }
+            activeElement = eve.target;
+            activeElement.classList.add('person-active');
+        }
+    })
+}
+formGung.addEventListener('click', (eve) => {
+    eve.preventDefault();
+})
 
 
+personShort();
 shortCurrent();
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,35 @@
+const selectDropDown = () => {
+  const dropdowns = document.querySelectorAll('.selectCustom');
+
+  dropdowns.forEach(dropDown => {
+    const select = dropDown.querySelector('.select');
+    const caret = dropDown.querySelector('.caret');
+    const menu = dropDown.querySelector('.menu');
+    const options = dropDown.querySelectorAll('.menu li');
+    const selected = dropDown.querySelector('.selected');
+
+    dropDown.addEventListener('click', (eve) => {
+
+      select.classList.toggle('select-clicked');
+      caret.classList.toggle('caret-rotate');
+      menu.classList.toggle('menu-open');
+    });
+    options.forEach(option => {
+      option.addEventListener('click', () => {
+        selected.innerText = option.innerText;
+        select.classList.remove('select-clicked');
+        caret.classList.remove('caret-rotate');
+        menu.classList.remove('menu-open');
+
+        options.forEach(option => {
+          option.classList.remove('active');
+        });
+        option.classList.add('active');
+      })
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const dialog = document.querySelector('[role="dialog"]');
   const btnBurger = document.querySelector('[data-burger="menu"]');
@@ -83,6 +115,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   };
 
+
+
+  selectDropDown();
 
   tabs('.galleries__tabs', '.tabs__head', '.tabs__body', '.tabs__caption', 'tabs__caption_active', 'tabs__content_active');
   tabs('.route__tabs', '.tabs__head', '.tabs__body', '.tabs__caption', 'tabs__caption_active', 'tabs__content_active');
