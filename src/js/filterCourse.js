3fa9759d616dd6323a8bfffbbb1466f232499a8b
itdelta-prog <delta@it-delta.ru>
2023-10-27 16:10:04 +0300

    pravki

--- src/js/filterCourse.js
+++ src/js/filterCourse.js
@@ -1,3 +1,4 @@
+let content = document.getElementById('gungs');
 async function submitFitler(param) {
 
   let formData = new FormData();
@@ -15,6 +16,26 @@ async function submitFitler(param) {
 }
 
 
+const contentAdd = (result) => {
+  content.innerHTML = '';
+  result.forEach((item) => {
+    content.insertAdjacentHTML('afterBegin', `<div class="p-[18px_18px_24px_18px] sm:p-[18px_18px_32px_18px] flex flex-col items-start sm:items-center gap-6 sm:gap-8 bg-[#1A1A19]">
+  <div class="py-8 px-[60px] sm:p-[43px_78px_42px_81px] bg-[#222220] w-[294px] sm:w-[380px]"><img class="w-[220px] h-[150px]" src=${item.avatar}></div>
+  <div class="max-w-[283px] sm:max-w-[350px]">
+    <h3 class="sm:text-xl text-base font-medium leading-4 sm:leading-6 text-white mb-[18px]">${item.name}</h3>
+    <p class="sm:text-base text-sm font-normal leading-[140%] text-[rgba(_255,_255,_255,_0.40)] mb-6">Программы, включающие обучение навыкам безопасного обращения с оружием, меткой и быстрой стрельбы...</p>
+    <a href="#" class="inline-block sm:text-base text-sm font-normal underline text-white mb-6">Подробнее</a>
+    <div class="w-full h-[1px] bg-[#2D2D2B]"></div>
+  </div>
+  <div class="flex justify-between items-center gap-x-4 sm:gap-16">
+    <p class="text-white text-base sm:text-xl font-normal leading-6">20 500 ₽</p>
+    <button class="flex justify-center items-start py-[18px] px-9 bg-[#272723] text-white text-base btn font-normal">Забронировать</button>
+  </div>
+</div>
+      `)
+  });
+}
+
 const filterCatalog = () => {
 
   let filterBtnGun = document.querySelectorAll('[data-gun]');
@@ -22,17 +43,11 @@ const filterCatalog = () => {
 
 
 
-  let content = document.getElementById('gungs');
 
-  console.log(content)
  // let filterBtn = document.querySelectorAll('[data-filter]');
   //let activeElemFilter = filterBtn[0];
   let activeElemGang = filterBtnGun[0];
 
-
-  if (activeElemGang) {
-    activeElemGang.classList.toggle('btn__active');
-  }
 
   // if (activeElemFilter) {
   //   activeElemFilter.classList.toggle('btn__active');
@@ -58,8 +73,13 @@ const filterCatalog = () => {
   //   });
   // });
 
+
   filterBtnVisit.addEventListener('click', async(eve) => {
-    eve.currentTarget.classList.toggle('btn__active');
+
+    if(eve.target.closest('.btn__custom')) {
+      eve.target.closest('.btn__custom').classList.toggle('active');
+    }
+    // eve.currentTarget.classList.toggle('active');
 
     if(eve.currentTarget.dataset.filter === 'false') {
       eve.currentTarget.setAttribute('data-filter', true)
@@ -68,67 +88,27 @@ const filterCatalog = () => {
       eve.currentTarget.setAttribute('data-filter', false)
     }
 
-
-
     let result = await submitFitler({firstVisit:filterBtnVisit.dataset.filter, type: activeElemGang?.dataset.gun});
-    content.innerHTML = '';
-    result.forEach((item) => {
-      content.insertAdjacentHTML('afterBegin', `<div class="p-[18px_18px_24px_18px] sm:p-[18px_18px_32px_18px] flex flex-col items-start sm:items-center gap-6 sm:gap-8 bg-[#1A1A19]">
-  <div class="py-8 px-[60px] sm:p-[43px_78px_42px_81px] bg-[#222220] w-[294px] sm:w-[380px]"><img class="w-[220px] h-[150px]" src=${item.avatar}></div>
-  <div class="max-w-[283px] sm:max-w-[350px]">
-    <h3 class="sm:text-xl text-base font-medium leading-4 sm:leading-6 text-white mb-[18px]">${item.name}</h3>
-    <p class="sm:text-base text-sm font-normal leading-[140%] text-[rgba(_255,_255,_255,_0.40)] mb-6">Программы, включающие обучение навыкам безопасного обращения с оружием, меткой и быстрой стрельбы...</p>
-    <a href="#" class="inline-block sm:text-base text-sm font-normal underline text-white mb-6">Подробнее</a>
-    <div class="w-full h-[1px] bg-[#2D2D2B]"></div>
-  </div>
-  <div class="flex justify-between items-center gap-x-4 sm:gap-16">
-    <p class="text-white text-base sm:text-xl font-normal leading-6">20 500 ₽</p>
-    <button class="flex justify-center items-start py-[18px] px-9 bg-[#272723] text-white text-base btn font-normal">Забронировать</button>
-  </div>
-</div>
-      `)
-    })
+    contentAdd(result);
   })
 
   document.querySelector('.nav-gung').addEventListener('click', async (event) => {
-    let filterBtn = document.querySelectorAll('[data-gun]');
-
-    if (activeElemGang) {
-      activeElemGang.classList.toggle('btn__active');
+    let el = event.target.closest('.btn__custom');
+    if(el) {
+      if(activeElemGang === el) {
+        activeElemGang.classList.remove('active');
+        activeElemGang = undefined;
+      }
+      else {
+        activeElemGang?.classList.remove('active');
+        activeElemGang = el;
+        activeElemGang.classList.add('active');
+      }
     }
 
-    filterBtn.forEach((item) => {
-      if (item.isEqualNode(event.target.parentNode)) {
-        if (activeElemGang?.isEqualNode(item)) {
-          item.classList.remove('btn__active');
-          activeElemGang = undefined;
-        }
-        else {
-          activeElemGang = item;
-          item.classList.toggle('btn__active');
-        }
-      }
-    });
 
     let result = await submitFitler({firstVisit: filterBtnVisit.dataset.filter, type: activeElemGang?.dataset.gun});
-
-    content.innerHTML = '';
-    result.forEach((item) => {
-      content.insertAdjacentHTML('afterBegin', `<div class="p-[18px_18px_24px_18px] sm:p-[18px_18px_32px_18px] flex flex-col items-start sm:items-center gap-6 sm:gap-8 bg-[#1A1A19]">
-  <div class="py-8 px-[60px] sm:p-[43px_78px_42px_81px] bg-[#222220] w-[294px] sm:w-[380px]"><img class="w-[220px] h-[150px]" src=${item.avatar}></div>
-  <div class="max-w-[283px] sm:max-w-[350px]">
-    <h3 class="sm:text-xl text-base font-medium leading-4 sm:leading-6 text-white mb-[18px]">${item.name}</h3>
-    <p class="sm:text-base text-sm font-normal leading-[140%] text-[rgba(_255,_255,_255,_0.40)] mb-6">Программы, включающие обучение навыкам безопасного обращения с оружием, меткой и быстрой стрельбы...</p>
-    <a href="#" class="inline-block sm:text-base text-sm font-normal underline text-white mb-6">Подробнее</a>
-    <div class="w-full h-[1px] bg-[#2D2D2B]"></div>
-  </div>
-  <div class="flex justify-between items-center gap-x-4 sm:gap-16">
-    <p class="text-white text-base sm:text-xl font-normal leading-6">20 500 ₽</p>
-    <button class="flex justify-center items-start py-[18px] px-9 bg-[#272723] text-white text-base btn font-normal">Забронировать</button>
-  </div>
-</div>
-      `)
-    })
+    contentAdd(result);
 
   });
 
--- src/js/react/FormReservation.jsx
+++ src/js/react/FormReservation.jsx
@@ -1,6 +1,4 @@
-
 function FormReservation({dataForm}) {
-    const person = [1, 2, 3, 4];
     const [dataReserv, setDataReserv] = React.useState({
         day: 'weekdays',
         basicPrice: dataForm.weekdays.basicPrice.price,
@@ -10,7 +8,9 @@ function FormReservation({dataForm}) {
         instructorPrice: 0,
         galleryPrice: 0,
     });
-    console.log(dataReserv);
+    const [active, setActive] = React.useState(false);
+    const person = [1, 2, 3, 4];
+    console.log(dataReserv)
     return (
         <React.Fragment>
             <div className="mb-[31px]">
@@ -19,7 +19,7 @@ function FormReservation({dataForm}) {
                         <span className="btn-link__text">Будни</span>
                     </button>
                     <button onClick={() => setDataReserv({...dataReserv, day:'weekend', basicPrice: dataForm.weekend.basicPrice.price, shoatsPrice: dataForm.weekend.shoatsPrice.price})} className={` btn-catalog basis-[184px] ${dataReserv.day === 'weekend' ? "tabs__caption_active"  : ''}`}>
-                        <span className="btn-link__text">Выходные</span>
+                        <span className="btn-link__text ">Выходные</span>
                     </button>
                 </div>
                 <div className="flex justify-between mb-[31px] person">
@@ -27,7 +27,7 @@ function FormReservation({dataForm}) {
                     {
                         person.map((item) => {
                             return (
-                                <button onClick={() => setDataReserv({...dataReserv, quantityPerson: item})} className={`${dataReserv.quantityPerson === item ? 'text-[#0F0F0F] bg-red-600 opacity-100' : 'hover:border-[#B8AA91]  border-white border-opacity-[0.4] hover:border-opacity-100 hover:text-[#B8AA91] hover:opacity-100 opacity-[0.4]'} transition-all bg-transparent px-6 py-4 border border-solid `}>{item}</button>
+                                <button onClick={() => setDataReserv({...dataReserv, quantityPerson: item})} className={`${dataReserv.quantityPerson === item ? 'text-[#0F0F0F] bg-[#B8AA91] border-transparent opacity-100' : 'hover:border-[#B8AA91] border-white border-opacity-[0.4] hover:border-opacity-100 hover:text-[#B8AA91] hover:opacity-100 opacity-[0.4]'} transition-all px-6 py-4 border border-solid`}>{item}</button>
                             )
                         })
                     }
@@ -46,6 +46,37 @@ function FormReservation({dataForm}) {
                 </div>
             </div>
             <div className="w-full h-[1px] bg-[#282828] mb-9"></div>
+            <div class="flex flex-col gap-y-[11px] mb-[31px]">
+
+                <div id="addService" class="flex flex-col gap-y-5">
+                    <button  onClick={() => {
+                        setActive(!active);
+                        active ? setDataReserv({...dataReserv, instructorPrice: Number(dataForm.personalInstructor)}) : setDataReserv({...dataReserv, instructorPrice: 0})
+                    }} className={`${active ? 'bg-red-500 after:opacity-0 before:opacity-0' : 'btn-catalog after:w-[calc(100%_-_10%)] group'} p-[12px_19px_12px_19px]  flex justify-between`}><span class={`${active ? 'text-white opacity-100' : 'opacity-20 group-hover:opacity-100'} text-[17px] btn-link__text`}>Персональный инструктор</span><span class="text-[17px] opacity-20 group-hover:opacity-100 btn-link__text" data-instructor="price">+{Number(dataForm.personalInstructor).toLocaleString()} р.</span></button>
+                    <div id="instructor" class="selectCustom disabled">
+                        <div class="select">
+                            <span class="selected">Выбрать инструктора</span>
+                            <div class="caret"></div>
+                        </div>
+                        <ul class="menu h-[200px] scrollCustom overflow-y-scroll">
+                            <li>Framer</li>
+                            <li>Sketch</li>
+                            <li>Figma</li>
+                        </ul>
+                    </div>
+                    <button onClick={() => dataReserv.galleryPrice === 0 ? setDataReserv({...dataReserv, galleryPrice: dataForm[dataReserv.day].personalGallery}) : setDataReserv({...dataReserv, galleryPrice: 0})} class="p-[12px_19px_12px_19px] after:w-[calc(100%_-_10%)] btn-catalog flex justify-between group btnService"><span class="text-[17px] opacity-20 group-hover:opacity-100 btn-link__text">Персональная галерея </span><span className="text-[17px] opacity-20  group-hover:opacity-100 btn-link__text" data-gallery="price">+{Number(dataForm[dataReserv.day].personalGallery).toLocaleString()} р.</span></button>
+                </div>
+            </div>
+            <div class="flex justify-between items-center mb-[21px]">
+                <p class="text-[18px] text-white">Итого</p>
+                <p class="text-[18px] text-white totalAmout">85 000 р.</p>
+            </div>
+            <div class="flex justify-between md:justify-normal md:gap-x-[13px]">
+                <button class="btn-catalog"><span
+                    class="btn-link__text">В подарок</span></button>
+                <button class="btn-catalog btnReserve">
+                    <span class="btn-link__text">К бронированию</span></button>
+            </div>
         </React.Fragment>
     )
 }
