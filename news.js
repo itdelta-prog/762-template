3edd33463d88fd0ec4d8766f4eaaebc1ce5bb414
itdelta-prog <delta@it-delta.ru>
2023-11-03 09:11:06 +0300

    Updates

--- news.js
+++
@@ -1,1 +0,0 @@
-import"./assets/script-df64ac74.js";new Swiper(".myProgram",{slidesPerView:1,breakpoints:{500:{slidesPerView:2,spaceBetween:20}},pagination:{el:".swiper-pagination__custom",type:"progressbar"}});
