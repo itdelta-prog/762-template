ae1df651099925a970325aad33379807dca76a31
itdelta-prog <delta@it-delta.ru>
2023-11-03 16:31:29 +0300

    Updates

--- contact.js
+++ contact.js
@@ -1,1 +1,1 @@
-import{T as t}from"./assets/tabs-9f3d8d9a.js";t(".route__tabs",".tabs__head",".tabs__body",".tabs__caption","tabs__caption_active","tabs__content_active");
+import"./assets/script-176bfda7.js";import{T as t}from"./assets/tabs-0d1d1a23.js";t(".route__tabs",".tabs__head",".tabs__body",".tabs__caption","tabs__caption_active","tabs__content_active");
---
+++ news.js
@@ -0,0 +1,1 @@
+import"./assets/script-176bfda7.js";new Swiper(".myProgram",{slidesPerView:1,breakpoints:{600:{slidesPerView:2,spaceBetween:20}},pagination:{el:".swiper-pagination__custom",type:"progressbar"}});
